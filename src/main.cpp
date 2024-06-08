1bea6c0d0ee113840f003462312d67a433be28e5
jalsol <jalsol@pm.me>
2024-06-08 21:31:35 +0700

    Main: Finish HTTP endpoints

--- src/main.cpp
+++ src/main.cpp
@@ -11,5 +11,22 @@ int main() {
         res.set_content(std::to_string(jaltop_ram_total()), "text/plain");
     });
 
+    server.Get("/ram/free", [&](const Request&, Response& res) {
+        res.set_content(std::to_string(jaltop_ram_free()), "text/plain");
+    });
+
+    server.Get("/ram/usage", [&](const Request&, Response& res) {
+        res.set_content(std::to_string(jaltop_ram_usage()), "text/plain");
+    });
+
+    server.Get("/cpu/usage", [&](const Request&, Response& res) {
+        res.set_content(std::to_string(jaltop_cpu_usage(0)), "text/plain");
+    });
+
+    server.Get("/cpu/usage/:interval", [&](const Request& req, Response& res) {
+        const auto interval = std::stoi(req.path_params.at("interval"));
+        res.set_content(std::to_string(jaltop_cpu_usage(interval)), "text/plain");
+    });
+
     server.listen("0.0.0.0", 6969);
 }
